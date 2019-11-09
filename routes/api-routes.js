5e96bf2b575df1b2ead73ed1435cd58a3cd1d0bc
Ivor Wallace <irwallacejr@gmail.com>
2019-11-09 12:19:18 -0500

    Added routes for update and delete.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,25 +12,51 @@ const db = require('../models');
 module.exports = function(app) {
   // GET route for getting all of the todos
   app.get('/api/todos', async (req, res) => {
-
+    const results = await db.Todo.findAll({});
+    res.json(results);
   });
 
   // POST route for saving a new todo.
   // We can create a todo using the data on req.body
   app.post('/api/todos', async (req, res) => {
-
+    const result = await db.Todo.create({
+      text: req.body.text,
+      complete: req.body.complete,
+    });
+    res.json({id: result.id});
   });
 
   // DELETE route for deleting todos.
   // We can access the ID of the todo to delete in
   // req.params.id
   app.delete('/api/todos/:id', async (req, res) => {
+    const result = await db.Todo.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
+    if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
   });
 
   // PUT route for updating todos.
   // We can access the updated todo in req.body
   app.put('/api/todos', async (req, res) => {
+    const result = await db.Todo.update({
+      text: req.body.text,
+    },
+    {
+      returning: true, where: {id: req.body.id},
+    });
 
+    if (result.updatedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
   });
 };
